c8cb74b9ea6c7c265ad20eb85ce57a454c5ac1fb
litecuts <71206543+litecuts@users.noreply.github.com>
2022-06-21 13:15:40 -0400

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,10 +3,10 @@ module.exports = {
 	description: 'Sends the avatar image of the user tagged.',
 	execute(message) {
         	const user = message.mentions.users.first() || message.author;
-    		const avatarEmbed = new Discord.RichEmbed()
-        	.setColor(0x333333)
-        	.setAuthor(user.username)
-        	.setImage(user.avatarURL);
+        const avatarEmbed = new Discord.RichEmbed()
+            .setColor(0x333333)
+            .setAuthor(user.username)
+            .setImage(user.avatarURL);
         message.channel.send(avatarEmbed);
 	},
 };
